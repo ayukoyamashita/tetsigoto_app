e43a5ad2257ddf4858bc12483679069f31d6bebb
山下彩友子 <yamashita-ayuko@esz.co.jp>
2020-01-27 23:56:19 +0900

    jsQR導入

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,8 @@
 const { environment } = require('@rails/webpacker')
-
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+	new webpack.ProvidePlugin({
+		jsQR: 'jsqr'
+	})
+)
 module.exports = environment
