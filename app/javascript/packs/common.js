9b0dd71bf186004d4b0a6397f12cadf598017a67
山下彩友子 <yamashita-ayuko@esz.co.jp>
2020-01-28 01:05:52 +0900

    画像が大きい場合は縮小

--- app/javascript/packs/common.js
+++ app/javascript/packs/common.js
@@ -5,15 +5,30 @@ const qrDecode = document.getElementById("qrDecode");
 
 camera.onchange = openQRCamera;
 
+const THUMBNAIL_WIDTH = 500; // 画像リサイズ後の横の長さの最大値
+const THUMBNAIL_HEIGHT = 500; // 画像リサイズ後の縦の長さの最大値
+
 function openQRCamera() {
 	var reader = new FileReader();
 	var image = new Image();
 	reader.onload = function (evt) {
 		image.onload = function () {
-			canvas.setAttribute("width", image.width);
-			canvas.setAttribute("height", image.height);
+			var width, height;
+			if(image.width > image.height){
+				// 横長の画像は横のサイズを指定値にあわせる
+				var ratio = image.height/image.width;
+				width = THUMBNAIL_WIDTH;
+				height = THUMBNAIL_WIDTH * ratio;
+			} else {
+				// 縦長の画像は縦のサイズを指定値にあわせる
+				var ratio = image.width/image.height;
+				width = THUMBNAIL_HEIGHT * ratio;
+				height = THUMBNAIL_HEIGHT;
+			}
+			canvas.setAttribute("width", width);
+			canvas.setAttribute("height", height);
 			ctx = canvas.getContext("2d");
-			ctx.drawImage(image, 0, 0); //canvasに画像を転写
+			ctx.drawImage(image,0,0,image.width,image.height,0,0,width,height);
 			checkPicture();
 		};
 		image.src = evt.target.result;
