d421d293b8fa52588f81a2eca63a6b126fa6ecf8
山下彩友子 <yamashita-ayuko@esz.co.jp>
2020-01-29 12:10:31 +0900

    CSS

--- app/javascript/packs/common.js
+++ app/javascript/packs/common.js
@@ -1,34 +1,32 @@
-let camera = document.getElementById('camera');
+const camera = document.getElementById('camera');
 const canvas = document.getElementById("picture");
-let ctx = canvas.getContext("2d");
+const ctx = canvas.getContext("2d");
 const qrDecode = document.getElementById("qrDecode");
-
-camera.onchange = openQRCamera;
-
 const THUMBNAIL_WIDTH = 500; // 画像リサイズ後の横の長さの最大値
 const THUMBNAIL_HEIGHT = 500; // 画像リサイズ後の縦の長さの最大値
 
+camera.onchange = openQRCamera;
+
 function openQRCamera() {
-	var reader = new FileReader();
-	var image = new Image();
+	let reader = new FileReader();
+	let image = new Image();
 	reader.onload = function (evt) {
 		image.onload = function () {
-			var width, height;
-			if(image.width > image.height){
+			let width, height;
+			if (image.width > image.height) {
 				// 横長の画像は横のサイズを指定値にあわせる
-				var ratio = image.height/image.width;
+				let ratio = image.height / image.width;
 				width = THUMBNAIL_WIDTH;
 				height = THUMBNAIL_WIDTH * ratio;
 			} else {
 				// 縦長の画像は縦のサイズを指定値にあわせる
-				var ratio = image.width/image.height;
+				let ratio = image.width / image.height;
 				width = THUMBNAIL_HEIGHT * ratio;
 				height = THUMBNAIL_HEIGHT;
 			}
 			canvas.setAttribute("width", width);
 			canvas.setAttribute("height", height);
-			ctx = canvas.getContext("2d");
-			ctx.drawImage(image,0,0,image.width,image.height,0,0,width,height);
+			ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, width, height);
 			checkPicture();
 		};
 		image.src = evt.target.result;
@@ -45,9 +43,7 @@ function checkPicture() {
 
 	// QRコードの読み取り
 	const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-	console.log(imageData);
 	const code = jsQR(imageData.data, canvas.width, canvas.height);
 
-	console.log(code.data);
 	qrDecode.value = code.data;
 }
