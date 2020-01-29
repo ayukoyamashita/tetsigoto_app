31bb1a6a792d9417fc8c53a8916bf694d9687e62
山下彩友子 <yamashita-ayuko@esz.co.jp>
2020-01-29 23:42:30 +0900

    CSS

--- app/javascript/packs/common.js
+++ app/javascript/packs/common.js
@@ -1,49 +1,62 @@
 const camera = document.getElementById('camera');
 const canvas = document.getElementById("picture");
-const ctx = canvas.getContext("2d");
 const qrDecode = document.getElementById("qrDecode");
-const THUMBNAIL_WIDTH = 500; // 画像リサイズ後の横の長さの最大値
-const THUMBNAIL_HEIGHT = 500; // 画像リサイズ後の縦の長さの最大値
+const success = document.querySelector('.stampCard_picture_text .el_success');
+const fail = document.querySelector('.stampCard_picture_text .el_fail');
+
+const ctx = canvas.getContext("2d");
 
 camera.onchange = openQRCamera;
+camera.onclick = resetCanvas;
 
 function openQRCamera() {
 	let reader = new FileReader();
 	let image = new Image();
 	reader.onload = function (evt) {
 		image.onload = function () {
-			let width, height;
-			if (image.width > image.height) {
-				// 横長の画像は横のサイズを指定値にあわせる
-				let ratio = image.height / image.width;
-				width = THUMBNAIL_WIDTH;
-				height = THUMBNAIL_WIDTH * ratio;
-			} else {
-				// 縦長の画像は縦のサイズを指定値にあわせる
-				let ratio = image.width / image.height;
-				width = THUMBNAIL_HEIGHT * ratio;
-				height = THUMBNAIL_HEIGHT;
-			}
+			let {width, height} = getThumbnailSize(image);
 			canvas.setAttribute("width", width);
 			canvas.setAttribute("height", height);
+
+			// カメラの映像をCanvasに複写
 			ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, width, height);
-			checkPicture();
+
+			// QRコード読み取り
+			const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+			const code = jsQR(imageData.data, canvas.width, canvas.height);
+
+			if (code == null) {
+				fail.classList.add('is-show');
+			} else {
+				success.classList.add('is-show');
+				qrDecode.value = code.data;
+			}
 		};
 		image.src = evt.target.result;
 	};
 	reader.readAsDataURL(camera.files[0]);
 }
 
-/**
- * QRコードの読み取り
- */
-function checkPicture() {
-	// カメラの映像をCanvasに複写
-	ctx.drawImage(canvas, 0, 0, canvas.width, canvas.height);
+function resetCanvas() {
+	fail.classList.remove('is-show');
+	success.classList.remove('is-show');
+}
 
-	// QRコードの読み取り
-	const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-	const code = jsQR(imageData.data, canvas.width, canvas.height);
+function getThumbnailSize(image) {
+	const THUMBNAIL_WIDTH = 500; // 画像リサイズ後の横の長さの最大値
+	const THUMBNAIL_HEIGHT = 500; // 画像リサイズ後の縦の長さの最大値
 
-	qrDecode.value = code.data;
+	let w, h;
+	if (image.width > image.height) {
+		// 横長の画像は横のサイズを指定値にあわせる
+		let ratio = image.height / image.width;
+		w = THUMBNAIL_WIDTH;
+		h = THUMBNAIL_WIDTH * ratio;
+	} else {
+		// 縦長の画像は縦のサイズを指定値にあわせる
+		let ratio = image.width / image.height;
+		w = THUMBNAIL_HEIGHT * ratio;
+		h = THUMBNAIL_HEIGHT;
+	}
+	return {width: w, height: h};
 }
