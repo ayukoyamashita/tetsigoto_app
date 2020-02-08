3602a2e55e864c94e7db004baaef2ff8da47d9aa
山下彩友子 <yamashita-ayuko@esz.co.jp>
2020-02-08 11:58:19 +0900

    no message

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
       user_name: ENV['SENDGRID_USERNAME'],
-      password: ENV['SENDGRID_API_KEY'],
+      password: ENV['SENDGRID_PASSWORD'],
       domain: 'herokuapp.com',
       address: 'smtp.sendgrid.net',
       port: 587,
