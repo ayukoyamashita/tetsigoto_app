d948034ef968d08815a821f5666459022197bf72
山下彩友子 <yamashita-ayuko@esz.co.jp>
2020-02-08 13:30:43 +0900

    no message

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,7 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'coccoboy_am@yahoo.co.jp'
+  config.mailer_sender = 'SOL STAMP'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
