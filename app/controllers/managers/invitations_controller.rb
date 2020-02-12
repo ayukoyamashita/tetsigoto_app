4b426bfd4390b9670d33bbf9c3483adb997009b5
山下彩友子 <yamashita-ayuko@esz.co.jp>
2020-02-13 00:41:19 +0900

    招待後のリダイレクト先

--- app/controllers/managers/invitations_controller.rb
+++ app/controllers/managers/invitations_controller.rb
@@ -1,6 +1,10 @@
 class Managers::InvitationsController < Devise::InvitationsController
 	layout :resolve_layout
 
+	def after_invite_path_for(inviter, invitee)
+		managers_path
+	end
+
 	def after_accept_path_for(resource)
 		managers_path
 	end
