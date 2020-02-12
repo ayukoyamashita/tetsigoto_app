f937c1a9ed95eabe3758c508048dc51391fc74d6
山下彩友子 <yamashita-ayuko@esz.co.jp>
2020-02-13 00:25:40 +0900

    no message

--- app/controllers/managers/invitations_controller.rb
+++ app/controllers/managers/invitations_controller.rb
@@ -1,6 +1,11 @@
 class Managers::InvitationsController < Devise::InvitationsController
 	layout :resolve_layout
 
+	def create
+		super
+		redirect_to new_manager_invitation_path
+	end
+
 	private
 
 	def resolve_layout
