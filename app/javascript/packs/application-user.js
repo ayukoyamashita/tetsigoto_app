15885212d462291acbfe10be2a86d42359e4b8eb
山下彩友子 <yamashita-ayuko@esz.co.jp>
2020-02-12 17:01:50 +0900

    cssとjsの構成変え

--- app/javascript/packs/application-user.js
+++ app/javascript/packs/application-user.js
@@ -9,7 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("jsqr")
 import "./qr_camera"
-import "../stylesheets/application"
+import "../stylesheets/application-user"
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
